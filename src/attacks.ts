027744e0a9adcb39a77d689f1dae53d0016289dc
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-08-17 11:31:29 +0200

    add an empty test case

---
+++ src/attacks.ts
@@ -0,0 +1,8 @@
+import { Sq, Board } from './types';
+
+const ROOK_DELTAS = [1, -1, 8, -8];
+const BISHOP_DELTAS = [7, -7, 9, -9];
+
+function slidingAttacks(square: Sq, deltas: number[], pieces: Board): Sq[] {
+  return [];
+}
---
+++ test/attacks.test.ts
@@ -0,0 +1,2 @@
+test('sliding attacks', () => {
+});
