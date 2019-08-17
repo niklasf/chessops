d137ac7685930b5c9710fdd233cb62ec9ffcad9d
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-08-17 15:50:42 +0200

    more fen test

--- test/fen.test.ts
+++ test/fen.test.ts
@@ -3,5 +3,9 @@ import { parse } from '../src/fen';
 test('read fen', () => {
   const pos = parse('chess', 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')!;
   expect(pos).toBeDefined();
+  expect(pos.board['e2']).toEqual({role: 'pawn', color: 'white'});
   expect(pos.turn).toBe('white');
+  expect(pos.epSquare).toBeUndefined();
+  expect(pos.halfmoves).toBe(0);
+  expect(pos.fullmoves).toBe(1);
 });
