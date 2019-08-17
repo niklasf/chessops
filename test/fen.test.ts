3b9260c483f77bbec842251c21d421bb25a9a796
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-08-17 17:10:46 +0200

    add some more tests

--- test/fen.test.ts
+++ test/fen.test.ts
@@ -2,7 +2,6 @@ import { parse } from '../src/fen';
 
 test('read fen', () => {
   const pos = parse('chess', 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')!;
-  expect(pos).toBeDefined();
   expect(pos.board['e2']).toEqual({role: 'pawn', color: 'white'});
   expect(pos.turn).toBe('white');
   expect(pos.epSquare).toBeUndefined();
@@ -12,4 +11,18 @@ test('read fen', () => {
 
 test('invalid fen', () => {
   expect(parse('chess', '8/8/8/8/8/8/8/8 w · - 0 1')).toBeUndefined();
+});
+
+test('remaining checks', () => {
+  const pos = parse('chess', '8/8/8/8/8/8/8/8 w - - 1+2 12 42')!;
+  expect(pos.remainingChecks).toEqual({ white: 1, black: 2});
+  expect(pos.halfmoves).toBe(12);
+  expect(pos.fullmoves).toBe(42);
+});
+
+test('lichess remaining checks', () => {
+  const pos = parse('chess', 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 1 2 +0+0')!;
+  expect(pos.remainingChecks).toEqual({ white: 3, black: 3});
+  expect(pos.halfmoves).toBe(1);
+  expect(pos.fullmoves).toBe(2);
 });
