262caed64a2e49b525dab34fbc18e0ee5f200c4a
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-08-20 19:48:11 +0200

    let makeMove update halfmoves and epSquare

--- src/makeMove.ts
+++ src/makeMove.ts
@@ -3,6 +3,7 @@ import { opposite, charToRole } from './util';
 import { KING_MOVES } from './attacks';
 
 export function makeMove(pos: Position, uci: Uci) {
+  pos.epSquare = undefined;
   if (pos.turn == 'black') pos.fullmoves++;
   const turn = pos.turn;
   pos.turn = opposite(turn);
@@ -23,15 +24,18 @@ export function makeMove(pos: Position, uci: Uci) {
   if (!piece) return;
 
   let capture = pos.board[to];
+  if (capture || piece.role == 'pawn') pos.halfmoves = 0;
+  else pos.halfmoves++;
 
-  // en passant
   if (piece.role == 'pawn') {
-    if (to == pos.epSquare) {
+    // en passant
+    if (!capture && from[1] != to[1]) {
       const fifthRank = to[0] + (turn == 'white' ? '6' : '4');
       capture = pos.board[fifthRank];
       delete pos.board[fifthRank];
     }
 
+    // set en passant square
     if (from[1] == '2' && to[1] == '4') pos.epSquare = from[0] + '3' as Square;
     else if (from[1] == '7' && to[1] == '5') pos.epSquare = from[0] + '6' as Square;
   }
