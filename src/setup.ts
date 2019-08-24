d172832c78cfa045bfe75d15146d60e40ddacac2
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-08-24 17:09:44 +0200

    use Result for setup

--- src/setup.ts
+++ src/setup.ts
@@ -1,16 +1,16 @@
+import { Result, ok, err } from './fp';
 import { Board, Setup, Position, Square, Colored } from './types';
 import { defined, opposite } from './util';
 import { findKing, attacksTo } from './attacks';
 
-export function setup(setup: Setup): Position | undefined {
-  console.log('----');
+export function setup(setup: Setup): Result<Position, string> {
   const board: Board = {};
   for (const square in setup.board) {
     const piece = setup.board[square];
     if (!piece) continue;
 
     // pawn on backrank
-    if (piece.role == 'pawn' && (square[1] == '1' || square[1] == '8')) return;
+    if (piece.role == 'pawn' && (square[1] == '1' || square[1] == '8')) return err('pawn on backrank');
 
     // copy and discard promoted flag
     board[square] = { role: piece.role, color: piece.color };
@@ -18,26 +18,26 @@ export function setup(setup: Setup): Position | undefined {
 
   // ep square
   if (defined(setup.epSquare)) {
-    if (setup.epSquare[1] != (setup.turn == 'white' ? '6' : '3')) return;
-    if (board[setup.epSquare]) return;
-    if (board[setup.epSquare[0] + (setup.turn == 'white' ? '7' : '2') as Square]) return;
+    if (setup.epSquare[1] != (setup.turn == 'white' ? '6' : '3')) return err('illegal ep square');
+    if (board[setup.epSquare]) return err('illegal ep square');
+    if (board[setup.epSquare[0] + (setup.turn == 'white' ? '7' : '2') as Square]) return err('illegal ep square');
     const pawn = board[setup.epSquare[0] + (setup.turn == 'white' ? '5' : '4') as Square];
-    if (!pawn || pawn.role != 'pawn' || pawn.color == setup.turn) return;
+    if (!pawn || pawn.role != 'pawn' || pawn.color == setup.turn) return err('illegal ep square');
   }
 
   // missing king
-  if (!defined(findKing(board, setup.turn))) return;
+  if (!defined(findKing(board, setup.turn))) return err('missing king');
 
   // other side in check
   const otherKing = findKing(board, opposite(setup.turn));
-  if (!defined(otherKing)) return;
-  if (attacksTo(board, setup.turn, otherKing).length) return;
+  if (!defined(otherKing)) return err('missing king');
+  if (attacksTo(board, setup.turn, otherKing).length) return err('opposite king in check');
 
-  return {
+  return ok({
     rules: 'chess',
     board,
     pockets: undefined,
     remainingChecks: undefined,
     ...setup
-  };
+  });
 }
--- test/setup.test.ts
+++ test/setup.test.ts
@@ -1,10 +1,11 @@
+import { unwrap } from '../src/fp';
 import { parseFen, INITIAL_FEN, EMPTY_FEN } from '../src/fen';
 import { setup } from '../src/setup';
 
 test('setup initial position', () => {
-  expect(setup(parseFen(INITIAL_FEN)!)).toBeDefined();
+  unwrap(setup(unwrap(parseFen(INITIAL_FEN))));
 });
 
 test('setup empty position', () => {
-  expect(setup(parseFen(EMPTY_FEN)!)).toBeUndefined();
+  expect(setup(unwrap(parseFen(EMPTY_FEN))).error).toBeDefined();
 });
