2b2046e313500a3c92f3d58ad28f643f290e5ff1
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-09-11 14:44:15 +0200

    generate step and ray tables

--- src/tables.ts
+++ src/tables.ts
@@ -28,7 +28,7 @@ function edges(square: Square): SquareSet {
 type BySquare<T> = { [square: number]: T };
 type Attacks = { [lo: number]: { [hi : number]: SquareSet } };
 
-function attackTable(deltas: number[]): [BySquare<SquareSet>, BySquare<Attacks>] {
+function slidingAttackTable(deltas: number[]): [BySquare<SquareSet>, BySquare<Attacks>] {
   const masks = [];
   const table = [];
   for (let square = 0; square < 64; square++) {
@@ -44,5 +44,57 @@ function attackTable(deltas: number[]): [BySquare<SquareSet>, BySquare<Attacks>]
   return [masks, table];
 }
 
-const [DIAG_MASKS, DIAG_ATTACKS] = attackTable([-9, -7, 7, 9]);
-const [ROOK_MASKS, ROOK_ATTACKS] = attackTable([-8, -1, 1, 8]);
+function stepAttackTable(deltas: number[]): BySquare<SquareSet> {
+  const table = [];
+  for (let square = 0; square < 64; square++) {
+    table[square] = slidingAttacks(square, SquareSet.full(), deltas);
+  }
+  return table;
+}
+
+export const KING_ATTACKS = stepAttackTable([-9, -8, -7, -1, 1, 7, 8, 9]);
+export const KNIGHT_ATTACKS = stepAttackTable([-17, -15, -10, -6, 6, 10, 15, 17]);
+export const PAWN_ATTACKS = {
+  white: stepAttackTable([7, 9]),
+  black: stepAttackTable([-7, -9])
+};
+
+const [DIAG_MASKS, DIAG_ATTACKS] = slidingAttackTable([-9, -7, 7, 9]);
+const [ROOK_MASKS, ROOK_ATTACKS] = slidingAttackTable([-8, -1, 1, 8]);
+
+export function bishopAttacks(square: Square, occupied: SquareSet): SquareSet {
+  const occ = DIAG_MASKS[square].intersection(occupied);
+  return DIAG_ATTACKS[square][occ.lo][occ.hi];
+}
+
+export function rookAttacks(square: Square, occupied: SquareSet): SquareSet {
+  const occ = ROOK_MASKS[square].intersection(occupied);
+  return ROOK_ATTACKS[square][occ.lo][occ.hi];
+}
+
+export function queenAttacks(square: Square, occupied: SquareSet): SquareSet {
+  return bishopAttacks(square, occupied).xor(rookAttacks(square, occupied));
+}
+
+function rayTables(): [BySquare<BySquare<SquareSet>>, BySquare<BySquare<SquareSet>>] {
+  const rays = {}, between = {};
+  for (let a = 0; a < 64; a++) {
+    rays[a] = [];
+    between[a] = [];
+    for (let b = 0; b < 64; b++) {
+      if (DIAG_ATTACKS[a][0][0].has(b)) {
+        rays[a][b] = DIAG_ATTACKS[a][0][0].intersection(DIAG_ATTACKS[b][0][0]).with(a).with(b);
+        between[a][b] = bishopAttacks(a, SquareSet.fromSquare(b)).intersection(bishopAttacks(b, SquareSet.fromSquare(a)));
+      } else if (ROOK_ATTACKS[a][0][0].has(b)) {
+        rays[a][b] = ROOK_ATTACKS[a][0][0].intersection(ROOK_ATTACKS[b][0][0]).with(a).with(b);
+        between[a][b] = rookAttacks(a, SquareSet.fromSquare(b)).intersection(rookAttacks(b, SquareSet.fromSquare(a)));
+      } else {
+        rays[a][b] = SquareSet.empty();
+        between[a][b] = SquareSet.empty();
+      }
+    }
+  }
+  return [rays, between];
+}
+
+export const [RAYS, BETWEEN] = rayTables();
