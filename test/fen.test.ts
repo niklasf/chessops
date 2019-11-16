5b4e019d4d32b22d59aa1536a9ecaf4623f01fd2
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-11-16 12:51:42 +0100

    test parse and make fen

--- test/fen.test.ts
+++ test/fen.test.ts
@@ -23,3 +23,15 @@ test('parse initial fen', () => {
   expect(setup.halfmoves).toEqual(0);
   expect(setup.fullmoves).toEqual(1);
 });
+
+test.each([
+  '8/8/8/8/8/8/8/8 w - - 1+2 12 42',
+  '8/8/8/8/8/8/8/8[Q] b - - 0 1',
+  'r3k2r/8/8/8/8/8/8/R3K2R[] w Qkq - 0 1',
+  'r3kb1r/p1pN1ppp/2p1p3/8/2Pn4/3Q4/PP3PPP/R1B2q~K1[] w kq - 0 1',
+  'rnb1kbnr/ppp1pppp/2Pp2PP/1P3PPP/PPP1PPPP/PPP1PPPP/PPP1PPP1/PPPqPP2 w kq - 0 1',
+  '5b1r/1p5p/4ppp1/4Bn2/1PPP1PP1/4P2P/3k4/4K2R w K - 1 1',
+])('parse and make fen', (fen) => {
+  const setup = parseFen(fen);
+  expect(makeFen(setup, { promoted: true })).toEqual(fen);
+});
