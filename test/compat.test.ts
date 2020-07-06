9a044bb2b0b571085fe0c36bd6444ebc371a4bf8
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-07-06 17:27:48 +0200

    update chessgroundDests test

--- test/compat.test.ts
+++ test/compat.test.ts
@@ -7,8 +7,8 @@ test('chessground dests with Kh8', () => {
   const setup = parseFen('r1bq1r2/3n2k1/p1p1pp2/3pP2P/8/PPNB2Q1/2P2P2/R3K3 b Q - 1 22').unwrap();
   const pos = Chess.fromSetup(setup).unwrap();
   const dests = chessgroundDests(pos);
-  expect(dests['g7']).toContain('h8');
-  expect(dests['g7']).not.toContain('g8');
+  expect(dests.get('g7')).toContain('h8');
+  expect(dests.get('g7')).not.toContain('g8');
 });
 
 test('uci char pair', () => {
