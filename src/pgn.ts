786fd1e38d6be82865123be747fc9b37e43fe5b9
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-04-15 10:42:41 +0200

    implement tree transform

--- src/pgn.ts
+++ src/pgn.ts
@@ -1,16 +1,51 @@
+import { defined } from './util.js';
+
 export interface Game<T> {
   headers: Map<string, string>;
   moves: Node<T>;
 }
 
 export class Node<T> {
-  children: ChildNode<T>[];
+  children: ChildNode<T>[] = [];
 }
 
 export class ChildNode<T> extends Node<T> {
   constructor(public data: T) {
     super();
   }
+}
+
+export function transform<T, U, C extends { clone(): C }>(
+  node: Node<T>,
+  ctx: C,
+  f: (ctx: C, data: T, i: number) => U | undefined
+): Node<U> {
+  const root = new Node<U>();
+  const stack = [
+    {
+      before: node,
+      after: root,
+      ctx,
+    },
+  ];
+  let frame;
+  while ((frame = stack.pop())) {
+    for (let i = 0; i < frame.before.children.length; i++) {
+      const ctx = i < frame.before.children.length - 1 ? frame.ctx.clone() : frame.ctx;
+      const childBefore = frame.before.children[i];
+      const data = f(ctx, childBefore.data, i);
+      if (defined(data)) {
+        const childAfter = new ChildNode(data);
+        frame.after.children.push(childAfter);
+        stack.push({
+          before: childBefore,
+          after: childAfter,
+          ctx,
+        });
+      }
+    }
+  }
+  return root;
 }
 
 export interface PgnNodeData {
