89a32345a771ec394df0acc5e29f73bfb5bcb6bf
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-04-18 22:51:27 +0200

    hint at error handling in read-pgn.js

--- examples/read-pgn.js
+++ examples/read-pgn.js
@@ -5,30 +5,37 @@ import { PgnParser, walk, startingPosition } from '../pgn.js';
 const validate = true;
 
 let games = 0;
+let errors = 0;
 let moves = 0;
 
 function status() {
-  console.log({ games, moves });
+  console.log({ games, moves, errors });
 }
 
+const stream = createReadStream(process.argv[2], { encoding: 'utf-8' });
+
 const parser = new PgnParser((game, err) => {
-  if (err) throw err;
+  games++;
+
+  if (err) {
+    errors++;
+    stream.destroy(err);
+  }
 
   if (validate)
     walk(game.moves, startingPosition(game.headers).unwrap(), (pos, node) => {
       const move = parseSan(pos, node.san);
-      if (!move) throw Error('illegal move');
-      else {
+      if (!move) {
+        errors++;
+        return false;
+      } else {
         pos.play(move);
         moves++;
       }
     });
 
-  games++;
   if (games % 1024 == 0) status();
 });
-
-const stream = createReadStream(process.argv[2], { encoding: 'utf-8' });
 
 stream.on('data', chunk => parser.parse(chunk, { stream: true }));
 stream.on('close', () => {
