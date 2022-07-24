8c54c7730e9b994a4efe514b521ec0ff7760e35b
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-07-24 21:54:24 +0200

    add roundtrip to fen fuzz test

--- fuzz/src/fen.fuzz.ts
+++ fuzz/src/fen.fuzz.ts
@@ -1,5 +1,16 @@
-import { parseFen } from 'chessops/fen';
+import { parseFen, makeFen } from 'chessops/fen';
+import { boardEquals } from 'chessops/board';
 
 export const fuzz = (data: Buffer): void => {
-  parseFen(data.toString()).map(console.log);
+  parseFen(data.toString()).map(setup => {
+    const roundtripped = parseFen(makeFen(setup)).unwrap();
+    if (!boardEquals(setup.board, roundtripped.board)) throw 'board not equal';
+    // TODO: check pockets
+    if (setup.turn !== roundtripped.turn) throw 'turn not equal';
+    if (!setup.unmovedRooks.equals(roundtripped.unmovedRooks)) throw 'unmoved rooks not equal';
+    if (setup.epSquare !== roundtripped.epSquare) throw 'ep square not equal';
+    // TODO: check remaining checks
+    if (setup.halfmoves !== roundtripped.halfmoves) throw 'halfmoves not equal';
+    if (setup.fullmoves !== roundtripped.fullmoves) throw 'fullmoves not equal';
+  });
 };
