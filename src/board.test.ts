5d03adf08d33defa0cf45b0d537a991b66b6cc30
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-08-04 10:42:15 +0200

    test rarely used board methods

--- src/board.test.ts
+++ src/board.test.ts
@@ -1,10 +1,21 @@
 import { Piece } from './types.js';
-import { Board } from './board.js';
+import { Board, boardEquals } from './board.js';
 
 test('set and get', () => {
-  const board = Board.empty();
-  expect(board.get(0)).toBeUndefined();
+  const emptyBoard = Board.empty();
+  expect(emptyBoard.getColor(0)).toBeUndefined();
+  expect(emptyBoard.getRole(0)).toBeUndefined();
+  expect(emptyBoard.has(0)).toBe(false);
+  expect(emptyBoard.get(0)).toBeUndefined();
+  expect(boardEquals(emptyBoard, emptyBoard.clone())).toBe(true);
+
+  const board = emptyBoard.clone();
   const piece: Piece = { role: 'knight', color: 'black', promoted: false };
   expect(board.set(0, piece)).toBeUndefined();
+  expect(board.getColor(0)).toBe('black');
+  expect(board.getRole(0)).toBe('knight');
+  expect(board.has(0)).toBe(true);
   expect(board.get(0)).toEqual(piece);
+  expect(boardEquals(board, board.clone())).toBe(true);
+  expect(boardEquals(emptyBoard, board)).toBe(false);
 });
