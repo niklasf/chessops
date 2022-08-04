4ca9a3a98b2a2f33094a73530556afe324ad5b7d
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-08-04 10:59:10 +0200

    test non-trivial compat functions

--- src/compat.test.ts
+++ src/compat.test.ts
@@ -11,10 +11,22 @@ test('chessground dests with Kh8', () => {
   expect(dests.get('g7')).not.toContain('g8');
 });
 
+test('chessground dests with regular castle', () => {
+  const setup = parseFen('r3k2r/8/8/8/8/8/8/R3K2R w KQkq - 0 1').unwrap();
+  const wtm = Chess.fromSetup(setup).unwrap();
+  expect(chessgroundDests(wtm).get('e1')!.sort()).toEqual(['a1', 'c1', 'd1', 'd2', 'e2', 'f1', 'f2', 'g1', 'h1']);
+  expect(chessgroundDests(wtm).get('e8')).toBeUndefined();
+
+  setup.turn = 'black';
+  const btm = Chess.fromSetup(setup).unwrap();
+  expect(chessgroundDests(btm).get('e8')!.sort()).toEqual(['a8', 'c8', 'd7', 'd8', 'e7', 'f7', 'f8', 'g8', 'h8']);
+  expect(chessgroundDests(btm).get('e1')).toBeUndefined();
+});
+
 test('chessground dests with chess960 castle', () => {
   const setup = parseFen('rk2r3/pppbnppp/3p2n1/P2Pp3/4P2q/R5NP/1PP2PP1/1KNQRB2 b Kkq - 0 1').unwrap();
   const pos = Chess.fromSetup(setup).unwrap();
-  expect(chessgroundDests(pos).get('b8')).toEqual(['a8', 'c8', 'e8']);
+  expect(chessgroundDests(pos).get('b8')!.sort()).toEqual(['a8', 'c8', 'e8']);
 });
 
 test('uci char pair', () => {
