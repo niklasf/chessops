1b64d73ac19304364fdb3ce51d31ef2cfba86e5a
Sam Ezeh <sam.z.ezeh@gmail.com>
2022-08-14 14:46:22 +0100

    Support having a header and move sequence on the same line

--- src/pgn.ts
+++ src/pgn.ts
@@ -466,9 +466,11 @@ export class PgnParser {
           this.consecutiveEmptyLines = 0;
           if (line.startsWith('[')) {
             this.consumeBudget(200);
-            const matches = line.match(/^\[([A-Za-z0-9_]+)\s+"(.*)"\]\s*$/);
-            if (matches) this.game.headers.set(matches[1], matches[2].replace(/\\"/g, '"').replace(/\\\\/g, '\\'));
-            return;
+            line = line.replace(/^\[([A-Za-z0-9_]+)\s+"(.*)"\]\s*/, (_match, headerName, headerValue) => {
+              this.game.headers.set(headerName, headerValue.replace(/\\"/g, '"').replace(/\\\\/g, '\\'));
+              return '';
+            });
+            if (line === '') return;
           }
           this.state = ParserState.Moves; // fall through
         case ParserState.Moves: {
