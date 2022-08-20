1b904938c9998efe6ec4025802404eb93499b229
Sam Ezeh <sam.z.ezeh@gmail.com>
2022-08-20 22:26:12 +0100

    Check header validity before searching for more headers

--- src/pgn.ts
+++ src/pgn.ts
@@ -465,17 +465,19 @@ export class PgnParser {
           this.found = true;
           this.consecutiveEmptyLines = 0;
           const trimmedLine = line.replace(/^\s+/, '');
+          let match = false;
           if (trimmedLine.startsWith('[')) {
             this.consumeBudget(200);
             line = trimmedLine.replace(
               /^\[([A-Za-z0-9_]+)\s+"((?:[^"\\]|\\"|\\\\)*)"\]\s*/,
               (_match, headerName, headerValue) => {
                 this.game.headers.set(headerName, headerValue.replace(/\\"/g, '"').replace(/\\\\/g, '\\'));
+                match = true;
                 return '';
               }
             );
-            if (line === '') return;
-            continue;
+            if (line === "") return;
+            if (match) continue;
           }
           this.state = ParserState.Moves; // fall through
         }
