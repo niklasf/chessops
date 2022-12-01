a35f1434b05918f6399af2575c6210b45cd1b35f
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-12-01 17:41:04 +0100

    update dev dependencies

--- examples/rollup.config.mjs
+++ examples/rollup.config.mjs
@@ -1,6 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: ['src/read-pgn.ts'],
